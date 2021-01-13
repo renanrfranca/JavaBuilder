1d877812ad486bd1a63ed92ecc4e41a0a1acda4a
Renan Rossignatti de França <renanrfranca@gmail.com>
2021-01-13 17:16:12 -0300

    feat: main class

--- src/com/renan/builder/Main.java
+++ src/com/renan/builder/Main.java
@@ -2,7 +2,44 @@ package com.renan.builder;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String promptMessage = "Informe o número da refeição desejada:\n" +
+            "1) Happy meal\n" +
+            "2) Chicken Special";
 
+    public static void main(String[] args) {
+        try {
+            validateInput(args);
+        } catch (Exception e) {
+            System.out.println(promptMessage);
+            return;
+        }
+
+        int mealCode = Integer.parseInt(args[0]);
+        CounterAttendant attendant = new CounterAttendant();
+        ChildMeal childMeal;
+
+        if (mealCode == 1) {
+            attendant.setChildMealBuilder(new MerryMealBuilder());
+        } else {
+            attendant.setChildMealBuilder(new ChickenSpecialBuilder());
+        }
+
+        attendant.buildChildMeal();
+        childMeal = attendant.getChildMeal();
+
+        System.out.println(childMeal);
+    }
+
+    public static void validateInput(String[] args) throws Exception {
+        int mealCode;
+
+        if (args.length == 1) {
+             mealCode = Integer.parseInt(args[0]);
+             if (mealCode > 0 && mealCode <= 2) {
+                 return;
+             }
+        }
+
+        throw new Exception("Código de refeição inválido");
     }
 }
